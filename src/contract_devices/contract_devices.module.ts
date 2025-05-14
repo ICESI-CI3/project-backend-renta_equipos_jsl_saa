4bc08dab2122673cc0d4c66852f283902b7e4bac
SebasL07 <sebasgnv0207@gmail.com>
2025-05-13 21:56:28 -0500

    fix

--- src/contract_devices/contract_devices.module.ts
+++ src/contract_devices/contract_devices.module.ts
@@ -5,10 +5,25 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ContractDevice } from "./entities/contract_device.entity";
 import { Contract } from "../contract/entities/contract.entity";
 import { Device } from "../devices/entities/device.entity";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
 
 @Module({
     providers: [ContractDevicesService],
     controllers: [ContractDevicesController],
-    imports: [TypeOrmModule.forFeature([ContractDevice, Contract, Device])],
+    imports: [
+        TypeOrmModule.forFeature([ContractDevice, Contract, Device]),
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+                JwtModule.registerAsync({
+                  imports: [ConfigModule],
+                  inject: [ConfigService],
+                  useFactory: (configService: ConfigService) => ({
+                    secret: configService.get('JWT_SECRET') as string || 'supersecretkey',
+                    signOptions: { expiresIn: '1h' },
+                  })
+                   
+                })
+    ],
     })
 export class ContractDevicesModule {}
