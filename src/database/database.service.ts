d8e18be35232ab57dec102a3076e91b5c20e5791
Samuel Alvarez Alban <115104098+Freddyedd21@users.noreply.github.com>
2025-06-05 16:11:39 -0500

    SAA/fix:update connecting database

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -8,7 +8,8 @@ export class DatabaseService {
     private readonly sql: NeonQueryFunction<false, false>;
 
     constructor(private configService: ConfigService) {
-        const databaseUrl = this.configService.get('DB_HOST');
+        // Use DATABASE_URL, not DB_HOST
+        const databaseUrl = this.configService.get('DATABASE_URL');
         this.sql = neon(databaseUrl);
     }
         async getData() {
