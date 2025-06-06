8e9e5c5805f0ac39c681a92da62c610c0c1c991b
SebasL07 <sebasgnv0207@gmail.com>
2025-06-05 20:59:17 -0500

    hotfix on cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000', 
-      'http://localhost:3001', 
-      process.env.NEXT_PUBLIC_BACKEND_URL?.replace('/api', '') || 'http://localhost:3000',
-    ].filter(Boolean),
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
